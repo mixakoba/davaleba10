3126504adc90793c67ab4ce27e3e3398f3dfc88b
nika <fantozy.v2@gmail.com>
2024-12-05 01:20:22 +0400

    added: Image model and Generic Relation between Item, Category and Image

--- shop/models.py
+++ shop/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    images = GenericRelation('Image', related_query_name="category")
 
     def __str__(self):
         return self.name
@@ -12,6 +16,7 @@ class Item(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='items')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, null=True)
+    images = GenericRelation("Image", related_query_name="item")
 
     def __str__(self):
         return self.name
@@ -22,3 +27,9 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+class Image(models.Model):
+    url = models.URLField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
